a0fd676bc560420d39c090bca0c2f56b62c45672
LucDutra <83315174+LucDutra@users.noreply.github.com>
2021-09-23 20:42:10 -0300

    AddComentario
    Adicionado comentario aos codigos.

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
+
 const transactionsUl = document.querySelector('#transactions') 
 
 
-
+// declaraccao de um objeto literal
 const dummyTransactions = [
     {id : 1, name : 'Bolo de brigadeiro', amount: -20},
     {id : 2, name : 'Salario', amount: 30},
@@ -9,8 +10,9 @@ const dummyTransactions = [
     {id : 3, name : 'Violao', amount: 150}
 ]
 
+
 const addTransactionIntoDom = transaction => {
-    const operator = transaction.amount < 0 ? '-' : '+';
+    const operator = transaction.amount < 0 ? '-' : '+'; // it ternario ou if linha
     const amountWithoutOperator = Math.abs(transaction.amount);
     const CSSclass = transaction.amount < 0 ? 'minus' : 'plus';
     const li = document.createElement('li');
@@ -24,22 +26,29 @@ const addTransactionIntoDom = transaction => {
     transactionsUl.append(li);
 }
 
+//Metodo que ira retornar 
 const updateBalanceValues = () => {
+
+    //pegar todos os valores do objeto dummy
     const transactionAmounts = dummyTransactions.map(transaction => transaction.amount);
 
-
+    //Somatorio dos valores retornados
     const total = transactionAmounts.reduce((acumulator, transaction) => acumulator + transaction, 0).toFixed(2);
+    console.log("Soma dos valores " + total)
 
-
+    // retorna todas as receitas
     const income = transactionAmounts.filter(values => values > 0).reduce((acumulator, values) => acumulator + values,0).toFixed(2);
+    console.log("calculo das receitas " + income)
 
-    const expense = Math.abs(transactionAmounts.filter(value => value < 0).reduce((acumulator, value) => acumulator + value, 0)).toFixed(2);
+    // retorna todas as despesas
+    const expense = Math.abs(transactionAmounts.filter(value => value < 0).reduce((acumulator, value) => acumulator - value, 0)).toFixed(2);
+    console.log("calc dos negativos " + expense)
 }
 
 
 
-
-const init = () =>{
+// inicializa todas as funções
+const init = () =>{   
     dummyTransactions.forEach(addTransactionIntoDom);
     updateBalanceValues();
 
