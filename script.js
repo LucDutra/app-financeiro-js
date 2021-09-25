86abfb3be5e79e47dc9aa7cddf6b6e3547aa1a9b
LucDutra <83315174+LucDutra@users.noreply.github.com>
2021-09-24 21:11:08 -0300

    Adicionado funcao de delete

--- script.js
+++ script.js
@@ -1,93 +1,99 @@
-
 // pegar componente por ID
 const transactionsUl = document.querySelector('#transactions');
-const moneyMinusDisplay = document.querySelector('#money-minus');
 const moneyPlusDisplay = document.querySelector('#money-plus');
+const moneyMinusDisplay = document.querySelector('#money-minus');
 const balanceDisplay = document.querySelector('#balance');
 const inputTransactionName = document.querySelector('#text');
 const inputTransactionAmount = document.querySelector('#amount');
 
+const formulario = document.querySelector('#form'); // pegando a referencia do formulario
+
 // declaraccao de um objeto literal
-const dummyTransactions = [
-    {id : 1, name : 'Bolo de brigadeiro', amount: -20},
-    {id : 2, name : 'Salario', amount: 30},
-    {id : 4, name : 'Bolo de Limao', amount: -20},
-    {id : 5, name : 'Violao', amount: 150},
-    {id : 6, name: 'Tenis', amount: 100}
+let dummyTransactions = [
+    { id: 1, name: 'Bolo de brigadeiro', amount: -20 },
+    { id: 2, name: 'Salario', amount: 30 },
+    { id: 3, name: 'Torta de limao', amount: -20 },
+    { id: 4, name: 'Violao', amount: 150 }
 ]
 
+// metodos para remover uma transacao da lista
+const removeTransaction = ID => {
+    dummyTransactions = dummyTransactions.filter(transaction => transaction.id !== ID);
+    console.log(dummyTransactions);
+    init();
+}
 
 const addTransactionIntoDom = transaction => {
-    const operator = transaction.amount < 0 ? '-' : '+'; // it ternario ou if linha
+    const operator = transaction.amount < 0 ? '-' : '+'; // if ternario ou if linha
     const amountWithoutOperator = Math.abs(transaction.amount);
-    const CSSclass = transaction.amount < 0 ? 'minus' : 'plus';
+    const CSSClass = transaction.amount < 0 ? 'minus' : 'plus';
     const li = document.createElement('li');
-    li.classList.add(CSSclass);
-
-    li.innerHTML = `
-    ${transaction.name} <span> ${operator} R$ ${amountWithoutOperator} </span> 
-    <button class="delete-btn">x</button>
-    `
-
+    li.classList.add(CSSClass);
+    li.innerHTML = `     
+                    ${transaction.name} <span>${operator} R$ ${amountWithoutOperator}</span>
+                    <button class="delete-btn" onClick="removeTransaction(${transaction.id})">x</button> `
     transactionsUl.append(li);
 }
 
-//Metodo que ira retornar 
+/*
+    Metodo que ira retornar todo os valores somados de receitas e despesas
+    */
 const updateBalanceValues = () => {
 
-    //pegar todos os valores do objeto dummy
+    // pegar todos os valores do objeto dummy
     const transactionAmounts = dummyTransactions.map(transaction => transaction.amount);
 
-    //Somatorio dos valores retornados
-    const total = transactionAmounts.reduce((acumulator, transaction) => acumulator + transaction, 0).toFixed(2);
-    
+    // somatorio dos valores retornados    
+    const total = transactionAmounts
+        .reduce((acumulator, transaction) => acumulator + transaction, 0)
+        .toFixed(2);
 
     // retorna todas as receitas
-    const income = transactionAmounts.filter(values => values > 0).reduce((acumulator, values) => acumulator + values,0).toFixed(2);
-    
+    const income = transactionAmounts
+        .filter(values => values > 0)
+        .reduce((acumulator, values) => acumulator + values, 0)
+        .toFixed(2);
 
     // retorna todas as despesas
-    const expense = Math.abs(transactionAmounts.filter(value => value < 0).reduce((acumulator, value) => acumulator - value, 0)).toFixed(2);
-   
+    const expense = transactionAmounts
+        .filter(values => values < 0)
+        .reduce((acumulator, values) => acumulator - values, 0)
+        .toFixed(2);
+
     balanceDisplay.textContent = `R$ ${total}`
     moneyPlusDisplay.textContent = `R$ ${income}`
     moneyMinusDisplay.textContent = `R$ ${expense}`
 
-
 }
-
-// inicializa todas as funções
-const init = () =>{   
+const init = () => { // inicializa todas as funções da pagina
+    transactionsUl.innerHTML = '';
     dummyTransactions.forEach(addTransactionIntoDom);
     updateBalanceValues();
-
+    inputTransactionName.value = '';
+    inputTransactionAmount.value = '';
 }
 
 init();
-
-// funcao que gera ID
+//  funcao que gera ID
 const generateId = () => Math.round(Math.random() * 1000);
 
-  
-form.addEventListener('submit', event =>{
+form.addEventListener('submit', event => {
     event.preventDefault();
 
+    // adicionado os componente a uma variavel para mellhor a escrita e reutilizacao
     const transactionName = inputTransactionName.value.trim();
     const transactionAmount = inputTransactionAmount.value.trim();
 
-    if(transactionName === "" && transactionAmount === ""){
-        alert('Informe os campos nome e valor da transação');
+    if (transactionName === "" && transactionAmount === "") {
+            alert('Informe os campos valor e nome da transação');
         return
-
     }
 
-const transaction = {
-    id: generateId(),
-    name: transactionName,
-    amount: transactionAmount
-}
+    //crio um objeto para fazer a insercao no array
+    const transaction = {id: generateId(),name: transactionName,amount: Number(transactionAmount)}
 
-console.log(transaction);
-
-   
+    // adiciono na lista na ultima posicao
+    dummyTransactions.push(transaction);
+    init();
 });
+
